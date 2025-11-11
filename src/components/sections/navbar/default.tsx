e636dbfdc4aac76ff6c6911272f564e6a5e1c2d9
Qyuzet <riqyuzet@gmail.com>
2025-11-11 15:17:34 +0700

    update

--- src/components/sections/navbar/default.tsx
+++ src/components/sections/navbar/default.tsx
@@ -101,11 +101,16 @@ export default function Navbar({
               </SheetTrigger>
               <SheetContent side="right">
                 <nav className="grid gap-6 text-lg font-medium">
-                  <Link
-                    href={homeUrl}
-                    className="flex items-center gap-2 text-xl font-bold"
-                  >
-                    <span>{name}</span>
+                  <Link href={homeUrl} className="flex items-center gap-2">
+                    {logo}
+                    <span className="text-xl font-bold">
+                      <span className="bg-gradient-to-r from-blue-600 to-blue-800 bg-clip-text text-transparent">
+                        Campus
+                      </span>
+                      <span className="bg-gradient-to-r from-teal-500 to-teal-700 bg-clip-text text-transparent">
+                        Circle
+                      </span>
+                    </span>
                   </Link>
                   <div className="mt-4">
                     <SignInButton
