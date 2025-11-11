71860c58d5265443787ee60df03a840bf1939c87
Qyuzet <riqyuzet@gmail.com>
2025-11-11 12:46:41 +0700

    feat: copy landing page template from example-ui with all components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import "./globals.css";
 import SessionProvider from "@/components/SessionProvider";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "CampusCircle - Binus University Student Marketplace",
@@ -31,7 +31,7 @@ export default function RootLayout({
           data-client-key={midtransClientKey}
         ></script>
       </head>
-      <body className={inter.className}>
+      <body>
         <SessionProvider>{children}</SessionProvider>
       </body>
     </html>
--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,36 +1,53 @@
-import * as React from "react"
-import { cva, type VariantProps } from "class-variance-authority"
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const badgeVariants = cva(
-  "inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  "inline-flex items-center rounded-full border border-border/100 dark:border-border/20 text-xs font-semibold transition-colors focus:outline-hidden focus:ring-2 focus:ring-ring focus:ring-offset-2 gap-2",
   {
     variants: {
       variant: {
         default:
-          "border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80",
+          "border-transparent bg-primary text-primary-foreground dark:shadow-sm dark:border-transparent",
+        brand:
+          "border-transparent bg-brand text-primary-foreground dark:shadow-sm dark:border-transparent",
         secondary:
-          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          "border-transparent bg-secondary text-secondary-foreground dark:shadow-sm dark:border-transparent",
         destructive:
-          "border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80",
+          "border-transparent bg-destructive/30 text-destructive-foreground dark:shadow-sm dark:border-transparent",
         outline: "text-foreground",
+      },
+      size: {
+        default: "px-2.5 py-1",
+        sm: "px-1",
       },
     },
     defaultVariants: {
       variant: "default",
+      size: "default",
     },
-  }
-)
+  },
+);
 
-export interface BadgeProps
-  extends React.HTMLAttributes<HTMLDivElement>,
-    VariantProps<typeof badgeVariants> {}
+function Badge({
+  className,
+  variant,
+  size,
+  asChild = false,
+  ...props
+}: React.ComponentProps<"span"> &
+  VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
+  const Comp = asChild ? Slot : "span";
 
-function Badge({ className, variant, ...props }: BadgeProps) {
   return (
-    <div className={cn(badgeVariants({ variant }), className)} {...props} />
-  )
+    <Comp
+      data-slot="badge"
+      className={cn(badgeVariants({ variant, size }), className)}
+      {...props}
+    />
+  );
 }
 
-export { Badge, badgeVariants }
+export { Badge, badgeVariants };
